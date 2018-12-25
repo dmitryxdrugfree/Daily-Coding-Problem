b48313099064f965c204e0904e18ef8446852a7b
dmitryxdrugfree <igonnariseabove@gmail.com>
2018-12-25 23:54:05 +0300

    simple refactoring

---
+++ problem_018/problem_018.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 22:39:32 2018
+
+@author: BigBoss
+"""
+
+def maxInSubArray(numbers, k):
+    ''' 
+    >>> maxInSubArray([10, 5, 2, 7, 8, 7], 3)
+    [10, 7, 8, 8]
+    >>> maxInSubArray([10, 5, 2, 7, 8, 7], 4)
+    [10, 8, 8]
+    >>> maxInSubArray([10, 5, 2, 7, 8, 7], 5)
+    [10, 8]
+    >>> maxInSubArray([10, 5, 2, 7, 8, 7], 2)
+    [10, 5, 7, 8, 8]
+    '''
+
+    counter = 0
+    while k!=len(numbers)+1:
+        numbers[counter] = max(numbers[counter:k])
+        k+=1
+        counter+=1
+    return numbers[:counter]
+
+print(maxInSubArray([10, 5, 2, 7, 8, 7], 3))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=False)
